50ce8ff1b861b577f3da13cf72a737581cbb94e5
Callum Nelson <callum.nelson95@gmail.com>
2023-04-03 20:15:09 -0400

    Update GUI and album > lyric logic

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@ import lyrics from '../data/lyrics.json' assert { type: 'json'}
 
 /*------------ Variables ------------*/
 const albums = []
+const songs = []
 let randLyrics = []
 let streak
 
 
 /*---- Cached Element References ----*/
 const lyricBtnEl = document.querySelector('#lyric-button')
-const lyricEl = document.querySelector('#current-lyric')
 const lyricWrapEl = document.querySelector('#lyric-wrapper')
 const albumsEl = document.querySelector('#album-wrapper')
+const songsEl = document.querySelector('#song-wrapper')
 const hintBtnEl = document.querySelector('#hint-button')
 
 /*--------- Event Listeners ---------*/
@@ -26,6 +27,9 @@ function getRandomLyric(){
   randLyrics = []
   let currentIndex = Math.floor(Math.random()*lyrics.length)
   randLyrics.push(lyrics[currentIndex])
+  console.log(lyrics[currentIndex])
+  songs.splice(0, songs.length)
+  setSongs(lyrics[currentIndex])
   render()
 }
 
@@ -42,21 +46,32 @@ function getHint() {
       hintLyric = lyrics[currentIndex + 1]
       randLyrics.push(hintLyric)
     }
-    console.log(randLyrics)
     render()
   }
 }
 
 function checkAnswer(evt){
-  console.dir(evt.target)
-  if (albums[+evt.target.id] === currentAlbum) {
-    evt.target.style.backgroundColor = 'green'
-    evt.target.style.color = 'white'
-  }else {
-    evt.target.style.backgroundColor = 'red'
-    evt.target.style.color = 'gray'
+  if (randLyrics.length){
+    if (evt.target.className === 'album') {
+      if (albums[+evt.target.id.split('-')[1]] === randLyrics[0].album_name) {
+        evt.target.style.backgroundColor = 'green'
+        evt.target.style.color = 'white'
+        showSongs()
+      }else {
+        evt.target.style.backgroundColor = 'red'
+        evt.target.style.color = 'gray'
+      }
+    } else if (evt.target.className === 'song'){
+      if (songs[+evt.target.id.split('-')[1]] === randLyrics[0].track_title) {
+        evt.target.style.backgroundColor = 'green'
+        evt.target.style.color = 'white'
+      }else {
+        evt.target.style.backgroundColor = 'red'
+        evt.target.style.color = 'gray'
+      }
+    }
   }
-  
+
 }
 
 function setAlbums(){
@@ -64,6 +79,14 @@ function setAlbums(){
     if (!a.includes(c.album_name)) a.push(c.album_name)
     return a
   }, albums)
+}
+
+function setSongs(currLyric){
+  let currLyrAlb = currLyric.album_name
+  lyrics.reduce( (a, c) => {
+    if (!a.includes(c.track_title) && c.album_name === currLyrAlb) a.push(c.track_title)
+    return a
+  }, songs)
 }
 
 function showLyric() {
@@ -78,10 +101,21 @@ function showAlbums() {
   albums.forEach( (album, idx) => {
     let albumEl = document.createElement('p')
     albumEl.className = 'album'
-    albumEl.id = idx
+    albumEl.id = `${album}-${idx}`
     albumEl.textContent = album
     albumsEl.appendChild(albumEl)
   })
+}
+
+function showSongs() {
+  songs.forEach( (song, idx) => {
+    let songEl = document.createElement('p')
+    songEl.className = 'song'
+    songEl.id = `${song}-${idx}`
+    songEl.textContent = song
+    songsEl.appendChild(songEl)
+  })
+  document.getElementById('song-header').style.display = 'flex'
 }
 
 function render(){
